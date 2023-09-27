f55fbd8311879c5f528f76ac0e234837e6ca893b
Michael Chapman <m5l.c5n@gmail.com>
2023-09-26 23:04:53 -0400

    layout page elements

--- src/App.svelte
+++ src/App.svelte
@@ -1,53 +1,188 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import { streetDictionary } from './streets'
 
-  let name = 'world'
+  const districts = {
+    1: {
+      location: 'West Hill School',
+      address: 'Cronin Drive'
+    },
+    2: {
+      location: 'Moser School',
+      address: '10 School Street'
+    },
+    3: {
+      location: 'Griswold Middle School',
+      address: '144 Bailey Road'
+    }
+  }
+
+  let filteredStreets = Object.keys(streetDictionary)
+
+  let selectedStreet
+  let selectedDistrict
+  let filterText = 'hello'
+
+  function resetFilterText() {
+    filterText = ''
+    filteredStreets = Object.keys(streetDictionary)
+  }
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+  <div class="mx-auto p-1">
+    <div class="flex flex-col">
+      <div class="flex flex-row gap-1">
+        <div class="basis-1/2">
+          {#if selectedDistrict}
+            <fieldset class="w-full rounded-md border border-gray-300 p-1">
+              <legend class="text-sm">District {selectedDistrict}</legend>
+              <p>{districts[selectedDistrict].location}</p>
+              <p>{districts[selectedDistrict].address}</p>
+            </fieldset>
+          {/if}
+        </div>
+        <div class="basis-1/2">
+          <label for="streets" class="block text-sm">Choose a street:</label>
+          <select
+            class="w-full rounded-md border border-gray-300"
+            id="streets"
+            name="streets"
+            size="10"
+            bind:value={selectedStreet}
+            on:change={() =>
+              (selectedDistrict = streetDictionary[selectedStreet])}
+          >
+            {#each filteredStreets as street}
+              <option value={street}> {street}</option>
+            {/each}
+          </select>
+        </div>
+      </div>
+      <div class="mt-2 flex gap-2">
+        <div class="flex-grow p-1 rounded-md border border-gray-300">
+          {#if filterText}
+            {filterText}
+          {:else}
+            <p>enter text to filter street list</p>
+          {/if}
+        </div>
+        <div>
+          <button
+            class="p-1 rounded-md border border-red-400 bg-red-200"
+            on:click={resetFilterText}
+            ><svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-6 h-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg></button
+          >
+        </div>
+      </div>
+      <div class="mt-2 flex flex-col">
+        <div class="flex flex-row justify-center">
+          <div class="p-1">
+            <button class="p-1 w-6 rounded-md border border-gray-300">q</button>
+          </div>
+          <div class="p-1">
+            <button class="p-1 w-6 rounded-md border border-gray-300">w</button>
+          </div>
+          <div class="p-1">
+            <button class="p-1 w-6 rounded-md border border-gray-300">e</button>
+          </div>
+          <div class="p-1">
+            <button class="p-1 w-6 rounded-md border border-gray-300">r</button>
+          </div>
+          <div class="p-1">
+            <button class="p-1 w-6 rounded-md border border-gray-300">t</button>
+          </div>
+          <div class="p-1">
+            <button class="p-1 w-6 rounded-md border border-gray-300">y</button>
+          </div>
+          <div class="p-1">
+            <button class="p-1 w-6 rounded-md border border-gray-300">u</button>
+          </div>
+          <div class="p-1">
+            <button class="p-1 w-6 rounded-md border border-gray-300">i</button>
+          </div>
+          <div class="p-1">
+            <button class="p-1 w-6 rounded-md border border-gray-300">o</button>
+          </div>
+          <div class="p-1">
+            <button class="p-1 w-6 rounded-md border border-gray-300">p</button>
+          </div>
+        </div>
+        <div class="flex flex-row justify-center">
+          <div class="p-1">
+            <button class="p-1 w-6 rounded-md border border-gray-300">a</button>
+          </div>
+          <div class="p-1">
+            <button class="p-1 w-6 rounded-md border border-gray-300">s</button>
+          </div>
+          <div class="p-1">
+            <button class="p-1 w-6 rounded-md border border-gray-300">d</button>
+          </div>
+          <div class="p-1">
+            <button class="p-1 w-6 rounded-md border border-gray-300">f</button>
+          </div>
+          <div class="p-1">
+            <button class="p-1 w-6 rounded-md border border-gray-300">g</button>
+          </div>
+          <div class="p-1">
+            <button class="p-1 w-6 rounded-md border border-gray-300">h</button>
+          </div>
+          <div class="p-1">
+            <button class="p-1 w-6 rounded-md border border-gray-300">j</button>
+          </div>
+          <div class="p-1">
+            <button class="p-1 w-6 rounded-md border border-gray-300">k</button>
+          </div>
+          <div class="p-1">
+            <button class="p-1 w-6 rounded-md border border-gray-300">l</button>
+          </div>
+        </div>
+        <div class="flex flex-row justify-center">
+          <div class="p-1">
+            <button class="p-1 w-6 rounded-md border border-gray-300">z</button>
+          </div>
+          <div class="p-1">
+            <button class="p-1 w-6 rounded-md border border-gray-300">x</button>
+          </div>
+          <div class="p-1">
+            <button class="p-1 w-6 rounded-md border border-gray-300">c</button>
+          </div>
+          <div class="p-1">
+            <button class="p-1 w-6 rounded-md border border-gray-300">v</button>
+          </div>
+          <div class="p-1">
+            <button class="p-1 w-6 rounded-md border border-gray-300">b</button>
+          </div>
+          <div class="p-1">
+            <button class="p-1 w-6 rounded-md border border-gray-300">n</button>
+          </div>
+          <div class="p-1">
+            <button class="p-1 w-6 rounded-md border border-gray-300">m</button>
+          </div>
+          <div class="p-1">
+            <button class="p-1 rounded-md border border-gray-300"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M12 9.75L14.25 12m0 0l2.25 2.25M14.25 12l2.25-2.25M14.25 12L12 14.25m-2.58 4.92l-6.375-6.375a1.125 1.125 0 010-1.59L9.42 4.83c.211-.211.498-.33.796-.33H19.5a2.25 2.25 0 012.25 2.25v10.5a2.25 2.25 0 01-2.25 2.25h-9.284c-.298 0-.585-.119-.796-.33z" />
+            </svg>
+            </button>
+          </div>
+        </div>
+
+      </div>
+    </div>
   </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a
-      href="https://github.com/sveltejs/kit#readme"
-      target="_blank"
-      rel="noreferrer">SvelteKit</a
-    >, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
-
-  <h1 class="text-3xl font-bold underline">Hello {name}!</h1>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
 </style>
